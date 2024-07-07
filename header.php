e3f063837e8ff3d056295310047c08aa3d1932f3
akila <toakila7@gmail.com>
2024-07-08 00:20:09 +0530

    add header and footer fragments

---
+++ header.php
@@ -0,0 +1,9 @@
+<div class="header">
+        <nav>
+            <ul>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="postcode.php">Post Code Manager</a></li>
+                <li><a href="#" onclick="showSignInPopup()">Sign In</a></li>
+            </ul>
+        </nav>
+</div>
